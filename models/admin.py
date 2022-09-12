9735950d036a52daa67cac67ca94ca8c70d21e12
JackCampi <bromeroa@unal.edu.co>
2022-09-11 20:57:23 -0500

    user model fixed

--- models/admin.py
+++ models/admin.py
@@ -4,9 +4,5 @@ class MainModel(BaseModel):
     """
     This is the description of the main model
     """
-
-    name: str 
     email: str
-    '''isAdmin: bool
-    isWaiter: bool'''
-    rol : str
+    sueldo: str
